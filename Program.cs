1f10a4b0149c8659a0cdf328c5ae421adfd99981
Erkan Yuksel <yukselerkan91@gmail.com>
2024-01-07 00:48:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // .Out parameters
+        string number = "999";
+        bool sonuc = int.TryParse(number, out int OutNumber);
+
+        if (sonuc)
+        {
+            Console.WriteLine("Succeded!");
+            Console.WriteLine(OutNumber);
+
+        }
+        else
+        {
+            Console.WriteLine("Error!");
+        }
+
+        Methods instance = new Methods();
+        instance.Sum(4, 5, out int totalResult);
+
+        Console.WriteLine(totalResult);
+
+        // Method Overloading
+        int ifade = 999;
+        instance.writeOnScreen(Convert.ToString(ifade));
+        instance.writeOnScreen(ifade);
+        instance.writeOnScreen("yuksel", "erkan");
+
+
+
+    }
+}
+
+class Methods
+{
+    public void Sum(int a, int b, out int SumTotal)
+    {
+        SumTotal = a + b;
+    }
+
+    public void writeOnScreen(string val)
+    {
+        Console.WriteLine(val);
+    }
+
+    public void writeOnScreen(int val)
+    {
+        Console.WriteLine(val);
+    }
+
+    public void writeOnScreen(string val, string val2)
+    {
+        Console.WriteLine(val + val2);
+    }
+}
